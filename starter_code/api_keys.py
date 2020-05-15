22ecc5f0e83d60ffc5ad2126148847e23ea698cd
Danne P <dannep@DanneParedesMBP.lan>
2020-05-14 21:32:39 -0700

    some progress

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "176a07033e9c8ec2aebbd4373e38ebf8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAPv7YNzTMSr0evqUDokrTXm4IUxlZN0tc"
